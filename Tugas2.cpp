54fb1dc6e1fc07d52feb90d138b5856f17ac5200
talithasalsabila0207-rgb <talithasalsabila0207@gmail.com>
2025-10-15 19:12:53 +0700

    Create Tugas2.cpp
    Data Diri

---
+++ Tugas2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Data diri
+    string nama     = "Talitha Salsabila";
+    string nim      = "254110601043";
+    string kampus   = "UIN Prof. K.H. Saifuddin Zuhri Purwokerto";
+    string prodi    = "Informatika";
+    string fakultas = "Dakwah & Saintek";
+    string kelas    = "Informatika A";
+    string ttl      = "Cilacap, 02 Juli 2007";
+    string umur     = "18 Tahun";
+    string alamat   = "Sampang, Cilacap, Jawa Tengah";
+    string hobi     = "Membaca novel";
+
+    // Output
+    cout << "=== PERKENALAN DIRI ===" << endl;
+    cout << "Nama      : " << nama << endl;
+    cout << "NIM       : " << nim << endl;
+    cout << "Kampus    : " << kampus << endl;
+    cout << "Prodi     : " << prodi << endl;
+    cout << "Fakultas  : " << fakultas << endl;
+    cout << "Kelas     : " << kelas << endl;
+    cout << "TTL       : " << ttl << endl;
+    cout << "Umur      : " << umur << endl;
+    cout << "Alamat    : " << alamat << endl;
+    cout << "Hobi      : " << hobi << endl;
+
+    return 0;
+}
