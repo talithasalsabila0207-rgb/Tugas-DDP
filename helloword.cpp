0268055928aea70f3fe49c95fb66c6ef44d14c00
talithasalsabila0207-rgb <talithasalsabila0207@gmail.com>
2025-10-15 19:05:34 +0700

    Create helloword.cpp

---
+++ helloword.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Nama : Talitha Salsabila" << endl;
+    cout << "NIM : 254110601043" << endl;
+    cout << "Kelas : Informatika A" << endl;
+    return 0;
+}
